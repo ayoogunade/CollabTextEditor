87cbdf625a0ca9493297fbb3b8a8bdfe782a3bf5
Ayo Ogunade <ekv137@usask.ca>
2025-01-02 02:49:16 -0600

    Updated server.js to use dynamic port (process.env.PORT)

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ wss.on('connection', (ws) => {
     });
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
+
