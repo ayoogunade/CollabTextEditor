8826a735d518e76a72d2df79a2e82a1ead4d4c1b
Ayo Ogunade <ekv137@usask.ca>
2025-01-02 03:15:05 -0600

    Fixed backend deployment configuration

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ wss.on('connection', (ws) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
